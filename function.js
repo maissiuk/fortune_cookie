b775e0661b03cf9ebdbfab4a5b04191cdeeb62a4
maissiuk <dana.maissiuk@gmail.com>
2025-02-11 19:40:39 +0200

    Delete function.js

--- function.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-   let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-   document.body.style.backgroundColor = randomColor;
-   document.getElementById("colorCode").innerText = randomColor;
-}
